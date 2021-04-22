b36754636c9856d179de265d6f23228956c5df9b
ecchost <ekosetio14@gmail.com>
2021-04-22 09:15:56 +0800

    Week 10

---
+++ src/firebase.config.js
@@ -0,0 +1,12 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBTrV-UeE8rENnucRnsoh0E5k56I64B5v4",
+  authDomain: "week-10-framework.firebaseapp.com",
+  projectId: "week-10-framework",
+  storageBucket: "week-10-framework.appspot.com",
+  messagingSenderId: "291443087475",
+  appId: "1:291443087475:web:49db95c28335168c2e7206",
+  measurementId: "G-EYF63PNEG6"
+};
+
+export default firebaseConfig
